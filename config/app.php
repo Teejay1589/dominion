fe8990a9a50a1703001889f9abb2731126f758ad
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2018-03-22 21:01:11 +0100

    PDF Package installation via composer

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Added Packages
+        Barryvdh\DomPDF\ServiceProvider::class,
+
     ],
 
     /*
@@ -226,6 +229,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        // Added Pakages
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
 ];
