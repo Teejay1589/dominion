92e52b8f1d6d178dbb1be033aa8da96c364179c3
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2018-09-18 11:18:12 +0100

    updated blog posts view with toolbar

--- resources/views/m/blog/posts/index.blade.php
+++ resources/views/m/blog/posts/index.blade.php
@@ -4,55 +4,20 @@
 @endsection
 
 @section('content')
-
-	{{-- <section>
-		<div class="container-fluid">
-
-
-		</div>
-
-		<div class="col-md-12">
-			<table class="table">
-				<thead>
-					<th>Title</th>
-					<th>Body</th>
-					<th>Created At</th>
-					<th></th>
-				</thead>
-
-				<tbody>
-
-					@foreach ($posts as $post)
-
-						<tr>
-							<td>{{ $post->title }}</td>
-							<td>{{ substr(strip_tags($post->body), 0, 50) }}{{ strlen(strip_tags($post->body)) > 50 ? "..." : "" }}</td>
-							<td>{{ date('M j, Y', strtotime($post->created_at)) }}</td>
-							{--<td><a href="{{ route('posts.show', $post->id) }}" class="btn btn-default btn-sm">View</a> <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default btn-sm">Edit</a></td>--}
-						</tr>
-
-					@endforeach
-
-				</tbody>
-			</table>
-
-			<div class="text-center">
-				{!! $posts->links(); !!}
-			</div>
-		</div>
-	</section> --}}
-
 	<section>
         <div class="row mb15">
             <div class="col-md-1 col-xs-12"></div>
             <div class="col-md-10 col-xs-12">
-            	<div>
-					<a href="#modal-create" class="btn btn-primary btn-block" data-toggle="modal">Create New Post</a>
-					@include('m.blog.posts.create')
-				</div>
-
-                <div class="clearfix"></div>
-                <br>
+            	@include('components.toolbar', [
+                    'model' => new App\Post(),
+                    'create_form' => 'm.blog.posts.create',
+                    'create_text' => 'Create New Post',
+                    'data_name' => 'post',
+                    'data' => $posts,
+                    'removed_keys' => array('id', 'user_id', 'slug', 'image', 'created_at', 'updated_at'),
+                    'added_keys' => array(),
+                    'addup_keys' => array()
+                ])
 
                 <div id="accordion" role="tablist" aria-multiselectable="true">
                     @forelse ($posts as $element)
