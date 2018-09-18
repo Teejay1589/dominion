e16ad7419050a12136ffda79854c616e8737883e
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2018-09-18 14:32:55 +0100

    created PatientFile model

---
+++ app/PatientFIle.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PatientFIle extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'patient_id', 'file_name', 'file', 'user_id',
+    ];
+
+    public static function table()
+    {
+        return 'patient_files';
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo('App\Patient', 'patient_id');
+    }
+}
