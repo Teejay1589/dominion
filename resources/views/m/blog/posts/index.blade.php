a3ef54108fb9dc41f0066e41823de7548e951e2d
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2018-09-18 11:42:49 +0100

    added tinymce and toolbar

--- resources/views/m/blog/posts/index.blade.php
+++ resources/views/m/blog/posts/index.blade.php
@@ -60,3 +60,31 @@
 
 
 @endsection
+
+@section('page_scripts')
+    <script type="text/javascript">
+        var base_url = '{{ url('/m/blog/posts') }}';
+    </script>
+    <script type="text/javascript" src="{{ asset('js/toolbar.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/tinymce/jquery.tinymce.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/tinymce/tinymce.min.js') }}"></script>
+    {{-- <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script> --}}
+    <script type="text/javascript">
+        tinymce.init({
+            selector: 'textarea',
+            height: 180,
+            theme: 'modern',
+            plugins: 'spellchecker code save print preview fullpage searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern help',
+            toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | removeformat',
+            image_advtab: true,
+            templates: [
+            { title: 'Test template', content: '<h2>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h2><p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p><h3>Header Level 3</h3><ol><li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li><li>Aliquam tincidunt mauris eu risus.</li></ol><blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote><h4>Header Level 4</h4><ul><li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li><li>Aliquam tincidunt mauris eu risus.</li></ul>' }
+            ],
+        content_css: [
+            '{{ asset('urban/vendor/bootstrap/dist/css/bootstrap.css') }}',
+            '{{ asset('urban/styles/urban.css') }}'
+        ],
+            object_resizing : 'img'
+        });
+    </script>
+@endsection
