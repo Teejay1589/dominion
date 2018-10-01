cbfce0658a9e9135e8bcece9d68c781d6fccecb3
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2018-10-01 14:26:02 +0100

    Reminder delete fix

--- app/Http/Controllers/Internal/ReminderController.php
+++ app/Http/Controllers/Internal/ReminderController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers\Internal;
 
 use App\Reminder;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReminderController extends InternalControl
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->page = collect();
+        $this->page->title = 'Reminders';
+        $this->page->view = 'm.reminders';
+        $this->middleware('auth:admin');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +29,10 @@ class ReminderController extends InternalControl
      */
     public function index()
     {
-        //
+        return view($this->page->view)
+            ->with('reminders', Reminder::latest()->paginate(isset($_GET['entries']) ? $_GET['entries'] : 10))
+            ->with('users', User::all())
+            ->with('page', $this->page);
     }
 
     /**
@@ -35,7 +53,18 @@ class ReminderController extends InternalControl
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'label' => 'required|string|min:2|max:180',
+        ]);
+        $request['user_id'] = Auth::id();
+        // $request['user_id'] = $request['user'];
+        $request['done'] = isset($request->done) ? 1 : 0;
+
+        $obj = new Reminder($request->all());
+        $obj->save();
+
+        session()->flash('success', 'New Reminder Added!');
+        return redirect()->back();
     }
 
     /**
@@ -67,9 +96,20 @@ class ReminderController extends InternalControl
      * @param  \App\Reminder  $reminder
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Reminder $reminder)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'label' => 'required|string|min:2|max:180',
+        ]);
+        $request['user_id'] = Auth::id();
+        // $request['user_id'] = $request['user'];
+        $request['done'] = isset($request->done) ? 1 : 0;
+
+        $obj = Reminder::findOrFail($id);
+        $obj->update($request->all());
+
+        session()->flash('success', 'Reminder Updated!');
+        return redirect()->back();
     }
 
     /**
@@ -78,8 +118,10 @@ class ReminderController extends InternalControl
      * @param  \App\Reminder  $reminder
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Reminder $reminder)
+    public function destroy($id)
     {
-        //
+        Reminder::findOrFail($id)->delete();
+        session()->flash('success', 'Reminder Deleted!');
+        return redirect()->back();
     }
 }
