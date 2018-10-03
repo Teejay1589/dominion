9562985bf2a06d0b3d6d0fda62e95dae48bbd258
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2018-10-03 15:05:45 +0100

    smsm inline view abstraction

--- resources/views/partials/sms-inline-view.blade.php
+++ resources/views/partials/sms-inline-view.blade.php
@@ -10,18 +10,20 @@
     <dd class="mb5">{{ $active_object->created_at }}</dd>
 </dl>
 
-<div class="clearfix"></div>
-<br>
+@if (get_class(Auth::user()) == "App\User")
+    <div class="clearfix"></div>
+    <br>
 
-<div class="h4"><strong>Patients</strong> <small><strong>{{ $active_object->sms_patients->count() }}</strong></small></div>
-<div>
-    @if ( $active_object->sms_patients->count() != 0 )
-        <ol>
-            @foreach ($active_object->sms_patients as $element)
-                <li class="mb5">{{ $element->patient->full_name() }} <span class="badge" title="Phone Number">{{ $element->patient->phone_number }}</span> {!! $element->patient->file_number(true) !!}</li>
-            @endforeach
-        </ol>
-    @else
-        <div class="text-danger">No Patients Found!</div>
-    @endif
-</div>
+    <div class="h4"><strong>Patients</strong> <small><strong>{{ $active_object->sms_patients->count() }}</strong></small></div>
+    <div>
+        @if ( $active_object->sms_patients->count() != 0 )
+            <ol>
+                @foreach ($active_object->sms_patients as $element)
+                    <li class="mb5">{{ $element->patient->full_name() }} <span class="badge" title="Phone Number">{{ $element->patient->phone_number }}</span> {!! $element->patient->file_number(true) !!}</li>
+                @endforeach
+            </ol>
+        @else
+            <div class="text-danger">No Patients Found!</div>
+        @endif
+    </div>
+@endif
