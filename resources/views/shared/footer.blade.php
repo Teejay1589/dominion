a1c90cc5ca3b2561b29c6aa5173ba64928e15add
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2018-10-31 13:30:20 +0100

    footer blade update again

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="content-footer" style="padding-left: 15px; position: relative;">
   <nav class="footer-right">
     <ul class="nav">
-      <li>
+      {{-- <li>
         <a href="javascript:;">Feedback</a>
-      </li>
+      </li> --}}
       <li>
         <a href="javascript:;" class="scroll-up">
           <i class="fa fa-angle-up"></i>
@@ -15,7 +15,7 @@
   <nav class="footer-left">
     <ul class="nav">
       <li>
-        <a href="javascript:;"><span>DOMINION</span></a>
+        <a href="javascript:;">DOMINION</a>
       </li>
       <li>
         <a href="javascript:;"><i class="fa fa-copyright"></i> 2018. All rights reserved</a>
