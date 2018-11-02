b1fa1e166a64adc04ab3863caf8b67ba9fcff9fd
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2018-11-02 16:04:04 +0100

    added permission to user view

--- resources/views/partials/user-inline-view.blade.php
+++ resources/views/partials/user-inline-view.blade.php
@@ -4,10 +4,12 @@
 
     <dt>Role</dt>
     <dd class="mb5"><span class="badge">{{ optional($element->role)->name }}</span></dd>
+    <dt>Permissions</dt>
+    <dd class="mb5"><strong class="text-success">{{ App\Permission::where('permit', '>=', $element->role_id)->count() + App\UserPermission::where('user_id', $element->id)->count() }}</strong></dd>
     <dt>First Name</dt>
     <dd class="mb5">{{ $element->first_name }}</dd>
     <dt>Last Name</dt>
-    <dd class="mb5">{{ $element->last_name }} <a href="{{ url('/m/users/password/reset/'.$element->id) }}" class="text-primary">reset password</a></dd>
+    <dd class="mb5">{{ $element->last_name }} <a href="{{ url('/m/users/password/reset/'.$element->id) }}" class="text-primary hidden-print">reset password</a></dd>
     <dt>Email</dt>
     <dd class="mb5">{{ $element->email }}</dd>
     <dt>Phone</dt>
